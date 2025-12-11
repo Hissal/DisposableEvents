57b2bd671bbac843fa06ccf3e0537450502d3909
Lassi Harjaluoma <hissalofficial@gmail.com>
2025-12-11 23:00:41 +0200

    Add unit tests for OneShotEventHandler functionality

---
+++ DisposableEvents.Tests/Events/OneShotEventHandlerTest.cs
@@ -0,0 +1,45 @@
+﻿using DisposableEvents;
+
+namespace DisposableEvents.Tests.Events;
+
+[TestSubject(typeof(OneShotEventHandler<>))]
+public class OneShotEventHandlerTest {
+    readonly OneShotEventHandler<int> sut;
+    readonly IEventHandler<int> innerHandlerSub;
+    
+    public OneShotEventHandlerTest() {
+        innerHandlerSub = Substitute.For<IEventHandler<int>>();
+        sut = new OneShotEventHandler<int>(innerHandlerSub);
+    }
+    
+    [Fact]
+    public void Handler_IsInvokedOnlyOnce() {
+        sut.Handle(1);
+        sut.Handle(2);
+        sut.Handle(3);
+        
+        innerHandlerSub.Received(1).Handle(1);
+        innerHandlerSub.DidNotReceive().Handle(2);
+        innerHandlerSub.DidNotReceive().Handle(3);
+    }
+    
+    [Fact]
+    public void Subscription_IsDisposedAfterHandle() {
+        var subscriptionSub = Substitute.For<IDisposable>();
+        sut.SetSubscription(subscriptionSub);
+        
+        sut.Handle(42);
+        
+        subscriptionSub.Received(1).Dispose();
+    }
+    
+    [Fact]
+    public void Subscription_IsDisposedImmediately_IfAlreadyInvoked() {
+        sut.Handle(99);
+        
+        var subscriptionSub = Substitute.For<IDisposable>();
+        sut.SetSubscription(subscriptionSub);
+        
+        subscriptionSub.Received(1).Dispose();
+    }
+}
